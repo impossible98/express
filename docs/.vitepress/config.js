d02c1fa85c09ddaefa49929a32d5a2162e998f99
Chris <42108459+impossible98@users.noreply.github.com>
2022-04-27 10:03:01 +0800

    feat: Init project

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -10,6 +10,8 @@ export default {
         { text: "Hello world", link: "/starter/hello-world" },
         { text: "Express 生成器", link: "/starter/generator" },
         { text: "基本路由", link: "/starter/basic-routing" },
+        { text: "静态文件", link: "/starter/static-files" },
+        { text: "常见问题及解答", link: "/starter/faq" },
       ],
     },
   },
