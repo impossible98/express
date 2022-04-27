73b5b2c09141d8d5b550c835c48ebaa0da9d27cc
Chris <42108459+impossible98@users.noreply.github.com>
2022-04-27 10:32:20 +0800

    feat: Init project

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -17,8 +17,8 @@ export default {
       "/guide/": [
         { text: "路由", link: "/guide/routing" },
         { text: "编写中间件", link: "/guide/writing-middleware" },
+        { text: "使用中间件", link: "/guide/using-middleware" },
         { text: "路由", link: "/guide/error-handling" },
-        { text: "路由", link: "/guide/using-middleware" },
       ],
     },
   },
