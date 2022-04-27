31076bac056466d811250224954c458528852f00
Chris <42108459+impossible98@users.noreply.github.com>
2022-04-27 09:12:39 +0800

    feat: Init project

---
+++ Makefile
@@ -0,0 +1,9 @@
+
+build: fmt
+	npm run docs:build
+
+dev:
+	npm run docs:dev
+
+fmt:
+	npm run fmt
---
+++ docs/.vitepress/config.js
@@ -0,0 +1,14 @@
+export default {
+  title: "Express",
+  themeConfig: {
+    nav: [
+      { text: "入门", link: "/starter/installing" },
+    ],
+    sidebar: {
+      "/starter/": [
+        { text: "安装", link: "/starter/installing" },
+        { text: "Hello world 示例", link: "/starter/hello-world" },
+      ],
+    },
+  },
+};
