c7a0fc2bae578c0023508b30425dd0b29560ab72
Chris <42108459+impossible98@users.noreply.github.com>
2022-04-27 11:26:54 +0800

    feat: Init project

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,23 +1,44 @@
-export default {
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
   lang: 'zh-CN',
   title: "Express",
   lastUpdated: true,
   themeConfig: {
     nav: [
-      { text: "入门", link: "/starter/installing" },
-      { text: "指南", link: "/guide/routing" },
+      { text: "文档", link: "/starter/installing" ,
+      activeMatch: '^/starter/'},
       { text: "API 参考", link: "/4x/api" },
     ],
     sidebar: {
-      "/starter/": [
+      "/starter/": getStarterSidebar(),
+      "/4x/": [
+        { text: "4.x API", link: "/4x/api" },
+        { text: "3.x API", link: "/3x/api" },
+      ],
+      "/3x/": [
+        { text: "3.x API", link: "/3x/api" },
+      ],
+    },
+  },
+})
+
+function getStarterSidebar() {
+  return [
+    {
+      text: '入门',
+      children: [
         { text: "安装", link: "/starter/installing" },
         { text: "Hello world", link: "/starter/hello-world" },
         { text: "Express 生成器", link: "/starter/generator" },
         { text: "基本路由", link: "/starter/basic-routing" },
         { text: "静态文件", link: "/starter/static-files" },
         { text: "常见问题及解答", link: "/starter/faq" },
-      ],
-      "/guide/": [
+      ]
+    },
+    {
+      text: '指南',
+      children: [
         { text: "路由", link: "/guide/routing" },
         { text: "编写中间件", link: "/guide/writing-middleware" },
         { text: "使用中间件", link: "/guide/using-middleware" },
@@ -28,14 +49,7 @@ export default {
         { text: "迁移到 Express 4", link: "/guide/migrating-4" },
         { text: "迁移到 Express 5", link: "/guide/migrating-5" },
         { text: "数据库集成", link: "/guide/database-integration" },
-      ],
-      "/4x/": [
-        { text: "4.x API", link: "/4x/api" },
-        { text: "3.x API", link: "/3x/api" },
-      ],
-      "/3x/": [
-        { text: "3.x API", link: "/3x/api" },
-      ],
-    },
-  },
-};
+      ]
+    }
+  ]
+}
